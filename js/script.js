b17280893c005b4f5fe0ac73613b84b8d3d32a0d
Parvina Odinaeva <odinaeva.parvina@gmail.com>
2022-11-17 19:47:17 +0000

    correct the code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-{   // przycisk przełączający tło
+{   
     let buttonContenerChangeColor = document.querySelector(".buttonContenerChangeColor");
     let mainContener = document.querySelector(".mainContener");
     let background = document.querySelector(".js-background");
@@ -10,13 +10,13 @@
 }
 
 
-{   // przycisk zmieniający zawartość (Pokaż:/Ukryj:) w zalezności od stanu = ternary
-    let showHideButton = document.querySelector(".js-showHide"); // przycisk
-    let mention = document.querySelector(".js-mention"); // dodatek
-    let word = document.querySelector(".js-word"); // słowo
-    
-    // klikamy w przycisk, przełączając klasę "hide"
-    // jeśli dodatek jest w stanie "hide", widnieje słowo "Pokaż:", w przeciwnym razie "Ukryj:"
+{   
+    let showHideButton = document.querySelector(".js-showHide"); 
+    let mention = document.querySelector(".js-mention");
+    let word = document.querySelector(".js-word");
+} 
+
+{  
     showHideButton.addEventListener("click", () => {
         mention.classList.toggle("hide");
         word.innerText = mention.classList.contains("hide") ? "Pokaż:" : "Ukryj:";
